26896df9166f3ad1804508863129de4ae3ffa42c
3dapi <emarhayyim@hotmail.com>
2025-06-18 17:15:05 +0900

    3d world

---
+++ GLC.D3D11_dev3_3d_world/MainApp.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#ifndef _MainApp_H_
+#define _MainApp_H_
+
+#include <d3d11.h>
+#include <DirectxMath.h>
+
+using DirectX::XMMATRIX;
+
+class MainApp
+{
+protected:
+	ID3D11VertexShader*		m_shaderVtx		{};
+	ID3D11PixelShader*		m_shaderPxl		{};
+	ID3D11InputLayout*		m_vtxLayout		{};
+	ID3D11Buffer*			m_bufVtx		{};
+	int						m_bufVtxCount	{};
+	ID3D11Buffer*			m_bufIdx		{};
+	int						m_bufIdxCount	{};
+	ID3D11Buffer*           m_cnstWorld		{};
+	ID3D11Buffer*           m_cnstView		{};
+	ID3D11Buffer*           m_cnstProj		{};
+	XMMATRIX				m_mtWorld		{};
+	XMMATRIX				m_mtView		{};
+	XMMATRIX				m_mtProj		{};
+public:
+	MainApp();
+	virtual ~MainApp();
+	virtual int Init();
+	virtual int Destroy();
+	virtual int Update();
+	virtual int Render();
+};
+
+
+#endif
