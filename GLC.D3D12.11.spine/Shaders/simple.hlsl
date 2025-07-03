c354f6a60a4ce61b76ecdf4b4d887e6623b9b5c6
3dapi <emarhayyim@hotmail.com>
2025-07-03 13:40:02 +0900

    상수 버퍼 2개

--- GLC.D3D12.11.spine/Shaders/simple.hlsl
+++ GLC.D3D12.11.spine/Shaders/simple.hlsl
@@ -1,16 +1,18 @@
-//--------------------------------------------------------------------------------------
-// Constant Buffer Variables
+
 Texture2D			gTexDif   : register(t0);              // diffuse texture
 SamplerState		gSmpLinear : register(s0);              // sampler state
 
 cbuffer MVP0 : register(b0)
 {
 	matrix mtWld;
+}
+
+cbuffer MVP0 : register(b1)
+{
 	matrix mtViw;
 	matrix mtPrj;
 }
 
-//--------------------------------------------------------------------------------------
 struct VS_IN
 {
     float4 p : POSITION;
@@ -24,9 +26,7 @@ struct PS_IN
 	float2 t : TEXCOORD;
 };
 
-//--------------------------------------------------------------------------------------
 // main vertex shader
-
 PS_IN main_vs(VS_IN v)
 {
 	PS_IN o = (PS_IN) 0;
@@ -38,13 +38,11 @@ PS_IN main_vs(VS_IN v)
 	return o;
 }
 
-//--------------------------------------------------------------------------------------
 // main pixel shader
-
 float4 main_ps(PS_IN v) : SV_Target0
 {
 	float4 finalColor;
 	finalColor = gTexDif.Sample( gSmpLinear, v.t );
-    finalColor = v.d;
+    finalColor *= v.d;
     return finalColor;
 }
